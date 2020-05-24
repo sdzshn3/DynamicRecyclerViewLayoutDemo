5856535af19b74a18e2719e6550acd8619314f79
sdzshn3 <sdzshn3@gmail.com>
2020-05-24 20:45:09 +0530

    change to coroutines

--- app/src/main/java/soa/work/scheduler/demo/MainActivity.kt
+++ app/src/main/java/soa/work/scheduler/demo/MainActivity.kt
@@ -5,13 +5,12 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.android.material.internal.ContextUtils.getActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import soa.work.scheduler.demo.retrofit.ApiClient
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -35,54 +34,40 @@ class MainActivity : AppCompatActivity() {
         )
 
         val apiService = ApiClient.getApiService()
-        val getEmployeesCall = apiService.getEmployees()
-        //Getting Employees list
-        getEmployeesCall.enqueue(object : Callback<List<Employee>> {
-            override fun onFailure(call: Call<List<Employee>>, t: Throwable) {
-                Toast.makeText(this@MainActivity, "Something went wrong " + t.localizedMessage, Toast.LENGTH_SHORT).show()
-            }
 
-            override fun onResponse(call: Call<List<Employee>>, employeesResponse: Response<List<Employee>>) {
-                if (employeesResponse.isSuccessful) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val employeesResponse = apiService.getEmployees()
+            val studentsResponse = apiService.getStudents()
 
-                    //Getting Students list
-                    val getStudentsCall = apiService.getStudents()
-                    getStudentsCall.enqueue(object : Callback<List<Student>> {
-                        override fun onFailure(call: Call<List<Student>>, t: Throwable) {
-                            Toast.makeText(this@MainActivity, "Something went wrong", Toast.LENGTH_SHORT).show()
+            withContext(Dispatchers.Main) {
+                try {
+                    if (employeesResponse.isSuccessful && studentsResponse.isSuccessful) {
+                        var lastPositionOfStudent = 0
+                        for (i in 0 until employeesResponse.body()?.size!!) {
+                            lastPositionOfStudent = i
+                            commonList.add(employeesResponse.body()!![i])
+
+                            // If students are less than employees, below try catch will catch the exception
+                            try {
+                                commonList.add(studentsResponse.body()!![i])
+                            } catch (e: IndexOutOfBoundsException) {}
                         }
 
-                        override fun onResponse(call: Call<List<Student>>, studentsResponse: Response<List<Student>>) {
-                            if (studentsResponse.isSuccessful) {
-                                var lastPositionOfStudent = 0
-                                for (i in 0 until employeesResponse.body()?.size!!) {
-                                    lastPositionOfStudent = i
-                                    commonList.add(employeesResponse.body()!![i])
-
-                                    // If students are less than employees, below try catch will catch the exception
-                                    try {
-                                        commonList.add(studentsResponse.body()!![i])
-                                    } catch (e: IndexOutOfBoundsException) {}
-                                }
-
-                                // IF employees are less than students, we add remaining remaining students here
-                                if (employeesResponse.body()?.size!! < studentsResponse.body()?.size!!) {
-                                    for (i in lastPositionOfStudent until studentsResponse.body()?.size!!) {
-                                        commonList.add(studentsResponse.body()!![i])
-                                    }
-                                }
-
-                                myAdapter.notifyDataSetChanged()
-                            } else {
-                                Toast.makeText(this@MainActivity, "Something went wrong", Toast.LENGTH_SHORT).show()
+                        // IF employees are less than students, we add remaining remaining students here
+                        if (employeesResponse.body()?.size!! < studentsResponse.body()?.size!!) {
+                            for (i in lastPositionOfStudent until studentsResponse.body()?.size!!) {
+                                commonList.add(studentsResponse.body()!![i])
                             }
                         }
 
-                    })
-                } else {
+                        myAdapter.notifyDataSetChanged()
+                    } else {
+                        Toast.makeText(this@MainActivity, "Something went wrong", Toast.LENGTH_SHORT).show()
+                    }
+                } catch (e: Throwable) {
                     Toast.makeText(this@MainActivity, "Something went wrong", Toast.LENGTH_SHORT).show()
                 }
             }
-        })
+        }
     }
 }
--- app/src/main/java/soa/work/scheduler/demo/retrofit/ApiService.kt
+++ app/src/main/java/soa/work/scheduler/demo/retrofit/ApiService.kt
@@ -1,14 +1,14 @@
 package soa.work.scheduler.demo.retrofit
 
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.GET
 import soa.work.scheduler.demo.Employee
 import soa.work.scheduler.demo.Student
 
 interface ApiService {
     @GET("5cdf27653000002b00430d14")
-    fun getEmployees(): Call<List<Employee>>
+    suspend fun getEmployees(): Response<List<Employee>>
 
     @GET("5cdf2f353000004600430d29")
-    fun getStudents(): Call<List<Student>>
+    suspend fun getStudents(): Response<List<Student>>
 }
